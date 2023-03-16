0fab17bc3c64c800ec34efd3653e8591f34b7d50
Zmmitch <zach.5923@gmail.com>
2023-03-15 23:26:16 -0400

    Add files via upload

---
+++ BestSellerBook.kt
@@ -0,0 +1,28 @@
+package com.codepath.bestsellerlistapp
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * The Model for storing a single book from the NY Times API
+ *
+ * SerializedName tags MUST match the JSON response for the
+ * object to correctly parse with the gson library.
+ */
+class BestSellerBook {
+
+    @JvmField
+    @SerializedName("original_title")
+    var title: String? = null
+
+    //TODO bookImageUrl
+    @SerializedName("backdrop_path")
+    var bookImageUrl: String? = null
+
+
+    //TODO description
+    @SerializedName("overview")
+    var description: String? = null
+
+
+    //TODO-STRETCH-GOALS amazonUrl
+}
